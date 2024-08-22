c8c9ab9f34100d64e14c19fd4c8c2327869f14d5
Faizbabu <shadabshadabansar9079@gmail.com>
2024-08-22 07:17:47 +0530

    Update 0prem7.js

--- Priyansh/commands/0prem7.js
+++ Priyansh/commands/0prem7.js
@@ -1,24 +1,26 @@
 const fs = require("fs");
 module.exports.config = {
-	name: "Asslam walikum",
+  name: "Rohit",
     version: "1.0.1",
-	hasPermssion: 0,
-	credits: "FAIZ ANSARI", 
-	description: "no prefix",
-	commandCategory: "no prefix",
-	usages: "abal",
+  hasPermssion: 0,
+  credits: "FAIZ ANSARI", 
+  description: "hihihihi",
+  commandCategory: "no prefix",
+  usages: "Rohit",
     cooldowns: 5, 
 };
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
-	var { threadID, messageID } = event;
-	if (event.body.indexOf("Asslamualaikum")==0 || event.body.indexOf("Asslam walikum")==0 || event.body.indexOf("ASALAMU ALAIKUM")==0 || event.body.indexOf("Asslam walikum")==0 || event.body.indexOf("Assalamuslikum")==0 || event.body.indexOf("SALAM ALAIKUM")==0 || event.body.indexOf("SALAMALEKUM")==0 || event.body.indexOf("Assalamu alakum good afternoon")==0 || event.body.indexOf("Assalam o alaikum")==0 || event.body.indexOf("ASSLAMO WALAIKUM")==0 || event.body.indexOf("Assalamualaikum sbko")==0 || event.body.indexOf("Assalamualaikum")==0) {
-		var msg = {
-				body: "Walakum assalam😇😇❤️",
-			}
-			api.sendMessage(msg, threadID, messageID);
-		}
-	}
-	module.exports.run = function({ api, event, client, __GLOBAL }) {
-
+  var { threadID, messageID } = event;
+  if (event.body.indexOf("rohit")==0 || event.body.indexOf("Rohit")==0 || event.body.indexOf("@Rohit")==0 || event.body.indexOf("ROHIT")==0) {
+    var msg = {
+        body: "❤️𝐘𝐄 𝐋𝐎 ROHIT 𝐉𝐈 𝐀 𝐆𝐀𝐈🙈",
+        attachment: fs.createReadStream(__dirname + `/FAIZ/rohit.jpg`)
+      }
+      api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("😇", event.messageID, (err) => {}, true)
+    }
   }
+  module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+        }
